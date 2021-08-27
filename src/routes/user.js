2638ec1ccb51a85b434f5bb5687e51d56a23bc82
catman0745 <54468544+catman0745@users.noreply.github.com>
2021-08-27 18:57:20 +0300

    Add update & delete user requests

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,4 +1,9 @@
 const express = require('express')
+const {
+  isValidObjectId,
+  Types: { ObjectId },
+} = require('mongoose')
+
 const { User } = require('../schemas')
 const { handleAsync, toDto } = require('./utils')
 
@@ -19,6 +24,35 @@ router.post(
     const user = new User(req.body)
     await user.save()
     res.sendStatus(201)
+  })
+)
+
+router.put(
+  '/:id',
+  handleAsync(async (req, res) => {
+    console.log([isValidObjectId(req.params.id) ? 'valid' : 'invalid', req.params.id])
+    if (!isValidObjectId(req.params.id)) {
+      res.status(400).send('Invalid user id')
+      return
+    }
+    const id = new ObjectId(req.params.id)
+
+    await User.findByIdAndUpdate(id, req.body, { runValidators: true })
+    res.sendStatus(200)
+  })
+)
+
+router.delete(
+  '/:id',
+  handleAsync(async (req, res) => {
+    if (!isValidObjectId(req.params.id)) {
+      res.status(400).send('Invalid user id')
+      return
+    }
+    const id = new ObjectId(req.params.id)
+
+    await User.findByIdAndDelete(id)
+    res.sendStatus(200)
   })
 )
 
